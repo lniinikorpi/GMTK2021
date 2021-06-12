2efecaeb8effa8262f2280f1bac248875d0f0b0c
lniinikorpi <lassi.n94@gmail.com>
2021-06-12 11:12:10 +0300

    - Player prefab - Player movement - Tileset

---
+++ Assets/_scripts/Player/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("Stats")]
+    public int maxHealth = 3;
+    public int movementSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_scripts/Player/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Player _player;
+    private Vector2 _movement;
+    private float _movementSpeed;
+    private Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GetComponent<Player>();
+        _movementSpeed = _player.movementSpeed;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        _movement = value.Get<Vector2>();
+    }
+    void MovePlayer()
+    {
+        Vector3 velocity = new Vector2(_movement.x, _movement.y) * _movementSpeed;
+        velocity = transform.TransformDirection(velocity);
+        _rb.velocity = velocity;
+    }
+}
